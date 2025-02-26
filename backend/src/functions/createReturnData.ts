5b0cc2180c7b6ef983c2d936c4899ab939360858
zakottewell <zakottewell@virginmedia.com>
2025-02-26 17:35:34 +0000

    removed most backend code in preperation for rewrite

--- backend/src/functions/createReturnData.ts
+++
@@ -1,7 +0,0 @@
-export default (statusMessage: string, data: any, error?: string) => {
-    return {
-        message: statusMessage,
-        data: data,
-        error: error
-    }
-}
--- backend/src/routes/api/v1/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import text from "./text";
-
-const router = express.Router();
-
-router.use("/text", text);
-
-export default router;
