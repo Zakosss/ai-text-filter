bb15003801dfa299bac6dacb0914f1590a2ef3cf
zakottewell <zakottewell@virginmedia.com>
2025-02-26 13:41:05 +0000

    remove prisma migration files and ignore them

--- backend/prisma/migrations/20250224233555_removed_explicit_bool/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `explicit` on the `word` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "word" DROP COLUMN "explicit";
